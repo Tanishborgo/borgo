f9bc94753960b8b1f2969d596b7cf89a3b1f4cbb
Tanishborgo <borgotanish@gmail.com>
2025-01-23 09:35:26 +0530

    Update Home.js

--- src/pages/container/Home.js
+++ src/pages/container/Home.js
@@ -469,20 +469,39 @@ export default function Home() {
                 <div className="display-none-sm position-absolute right-0 fig-2">
                     <Image src={Fill3} width="100%" height="100%" />
                 </div>
-               <div style={{ marginBottom: "60px", marginTop: "60px", padding: 10 }} className="div-center">
-                    <video
-                        width="560"
-                        height="315"
-                        controls
-                        autoPlay
-                        loop
-                        muted
-                        style={{ display: 'block', margin: '0 auto' }}
-                    >
-                        <source src="https://res.cloudinary.com/dmymftumw/video/upload/v1737604572/swiztr76qwo2llnnacj1.mp4" type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
-                </div>
+               <div
+  style={{
+    marginBottom: "60px",
+    marginTop: "60px",
+    padding: 10,
+    position: "relative",
+    width: "100%",
+    maxWidth: "560px",
+    marginLeft: "auto",
+    marginRight: "auto",
+  }}
+  className="div-center"
+>
+  <video
+    style={{
+      width: "100%",
+      height: "auto",
+      display: "block",
+      margin: "0 auto",
+    }}
+    controls
+    autoPlay
+    loop
+    muted
+  >
+    <source
+      src="https://res.cloudinary.com/dmymftumw/video/upload/v1737604572/swiztr76qwo2llnnacj1.mp4"
+      type="video/mp4"
+    />
+    Your browser does not support the video tag.
+  </video>
+</div>
+
             </Box>
 
             <Box className="position-relative">
